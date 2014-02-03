9ff114807db5a76b84423cb569c3bb5ee15a2db7
Laure Fischer <laurefischer27@gmail.com>
2014-02-03 17:17:44 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rapport::Application.config.secret_key_base = '1ae864ae975f65d3db5050e802f1d3d29a3065e68c4498e932dbf7ba331bd0037fe4e63caef95156aaf5ae0dfbe412e567c11a70f3a7207f82de375889676f9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rapport::Application.config.session_store :cookie_store, key: '_rapport_session'
