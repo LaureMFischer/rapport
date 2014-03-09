1f4987379fb81bd3f399a6d92683e89756e94aa0
Laure Fischer <laurefischer27@gmail.com>
2014-03-09 15:58:49 -0400

    removed user email index and requirement for user to enter an email upon sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ActiveRecord::Base
     super && provider.blank?
   end
 
-  #def email_required?
-    #super && provider.blank?
-  #end
+  def email_required?
+    super && provider.blank?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 20140205184334) do
     t.text     "secret"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
