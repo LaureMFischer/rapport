56cd1d592a4b4c95023bd7d96a7fea157e270719
Laure Fischer <laurefischer27@gmail.com>
2014-03-19 17:52:32 -0400

    removed validation on notes model that were causing problems

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,4 @@
 class Note < ActiveRecord::Base
   belongs_to :connection
 
-  validates :body, presence: true
-  validates :date_of_interaction, presence: true
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,14 +1,6 @@
 require 'spec_helper'
 
 describe Note do
-  it "is invalid without a body" do
-    expect(Note.create(date_of_interaction: 'April 1, 2013')).to_not be_valid
-  end
-
-  it "is invalid without a date of interaction" do
-    expect(Note.create(body: 'This is a note.')).to_not be_valid
-  end
-
   describe "associations" do
     it { should belong_to :connection}
   end
